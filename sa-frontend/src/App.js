cbe1becb14d12357313ad22a4eb429f42fb9d83f
Artem <artemlis2703@gmail.com>
2023-05-19 17:18:22 +0300

    Add instructions with commands

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://127.0.0.1:80/sentiment', {
+        fetch('http://127.0.0.1:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
